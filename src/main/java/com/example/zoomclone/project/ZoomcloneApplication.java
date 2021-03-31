e08841cf418159d3914356c236acba8a3bc8e283
Ritik Goel <ritik.goel@mountblue.tech>
2021-03-31 17:55:48 +0530

    initial commit

---
+++ src/main/java/com/example/zoomclone/project/ZoomcloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoomclone.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoomcloneApplication.class, args);
+    }
+
+}
